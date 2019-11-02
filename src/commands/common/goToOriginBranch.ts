1e4a1418a9241c987cedeed435c2c227911a435d
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2019-11-02 19:19:24 +0200

    x

--- src/commands/common/goToOriginBranch.ts
+++ src/commands/common/goToOriginBranch.ts
@@ -1,7 +1,7 @@
 import ITerminal from '../../interfaces/ITerminal';
 
 export default async function goToOriginBranch(terminal: ITerminal, branch: string) {
-    await terminal.execSync('git fetch --all');
-    await terminal.execSync(`git checkout ${branch}`);
-    await terminal.execSync('git pull');
+    await terminal.execSync('git fetch --all', { stdio: 'ignore' });
+    await terminal.execSync(`git checkout ${branch}`, { stdio: 'ignore' });
+    await terminal.execSync(`git pull origin ${branch}`, { stdio: 'ignore' });
 }
