97a90511ab87daaa7b4f0c035539069fde32b293
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2019-11-02 15:52:12 +0200

    Test,commit

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,9 @@
-const chalk = require('chalk');
+const { green, cyan } = require('chalk');
 const CLI = require('../dist/CLI').default;
 const cli = new CLI();
 
 cli.onAny((event, data) => {
-    console.log(`${event}: ${data}`);
+    console.log(`${green('xsc')} ${cyan(data)}`);
 });
 
 module.exports = cli;
