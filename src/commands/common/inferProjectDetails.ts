8d3a6f166027426ab4367b740d09da004eb7cb7e
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2020-01-04 20:20:12 +0200

    Reduced complexity

--- src/commands/common/inferProjectDetails.ts
+++ src/commands/common/inferProjectDetails.ts
@@ -3,28 +3,44 @@ import getLocalPackageFile from './getLocalPackageFile';
 
 export default async function inferProjectDetails(terminal: ITerminal): Promise<IProjectDetails> {
     try {
-        const packageFile = await getLocalPackageFile(terminal);
+        return await getNodeJSProjectDetails(terminal);
+    } catch (e) {
+        return await getCSharpProjectDetails(terminal);
+    }
+}
+
+async function getNodeJSProjectDetails(terminal: ITerminal) {
+    const packageFile = await getLocalPackageFile(terminal);
+
+    return {
+        type: ProjectType.NodeJS,
+        version: packageFile.version
+    } as IProjectDetails;
+}
+
+async function getCSharpProjectDetails(terminal: ITerminal) {
+    const dir = terminal.readdirSync('.');
+
+    for (const folder of dir) {
+        const projectDetails = tryGetCSharpProjectDetails(terminal, `./${folder}`);
+
+        if (projectDetails) {
+            return projectDetails;
+        }
+    }
+
+    throw new Error('Could not infer project details.');
+}
+
+function tryGetCSharpProjectDetails(terminal: ITerminal, path: string) {
+    try {
+        const assemblyInfo = terminal.readFileSync(`${path}/Properties/AssemblyInfo.cs`).toString();
 
         return {
-            type: ProjectType.NodeJS,
-            version: packageFile.version
+            type: ProjectType.CSharp,
+            version: assemblyInfo.match(/AssemblyVersion\(\"([^\"]+)\"/)[1].replace(/\.\d+$/, '')
         } as IProjectDetails;
-    } catch (e) {
-        const dir = terminal.readdirSync('.');
-
-        for (const folder of dir) {
-            try {
-                const assemblyInfo = terminal.readFileSync(`./${folder}/Properties/AssemblyInfo.cs`).toString();
-
-                return {
-                    type: ProjectType.CSharp,
-                    version: assemblyInfo.match(/AssemblyVersion\(\"([^\"]+)\"/)[1].replace(/\.\d+$/, '')
-                } as IProjectDetails;
-            } catch (e) { /* Ignore */ }
-        }
-
-        throw new Error('Could not infer project details.');
-    }
+    } catch (e) { /* Ignore */ }
 }
 
 export interface IProjectDetails {
--- src/commands/common/updateProjectVersion.ts
+++ src/commands/common/updateProjectVersion.ts
@@ -4,20 +4,28 @@ import getLocalPackageFile from './getLocalPackageFile';
 
 export default async function updateProjectVersion(terminal: ITerminal, version: string) {
     try {
-        const packageFileName = resolve(terminal.cwd, 'package.json');
-        const packageFile = await getLocalPackageFile(terminal);
-
-        packageFile.version = version;
-        terminal.writeFileSync(packageFileName, JSON.stringify(packageFile, null, '\t'));
+        await updateNodeJSProjectVersion(terminal, version);
     } catch (e) {
-        const dir = terminal.readdirSync('.');
+        await updateCSharpProjectVersion(terminal, version);
+    }
+}
 
-        for (const folder of dir) {
-            try {
-                let assemblyInfo = terminal.readFileSync(`./${folder}/Properties/AssemblyInfo.cs`).toString();
-                assemblyInfo = assemblyInfo.replace(/AssemblyVersion\(\"([^\"]+)\"/, `AssemblyVersion("${version}.0"`);
-                terminal.writeFileSync(`./${folder}/Properties/AssemblyInfo.cs`, assemblyInfo);
-            } catch (e) { /* Ignore */ }
-        }
+async function updateNodeJSProjectVersion(terminal: ITerminal, version: string) {
+    const packageFileName = resolve(terminal.cwd, 'package.json');
+    const packageFile = await getLocalPackageFile(terminal);
+
+    packageFile.version = version;
+    terminal.writeFileSync(packageFileName, JSON.stringify(packageFile, null, '\t'));
+}
+
+async function updateCSharpProjectVersion(terminal: ITerminal, version: string) {
+    const dir = terminal.readdirSync('.');
+
+    for (const folder of dir) {
+        try {
+            let assemblyInfo = terminal.readFileSync(`./${folder}/Properties/AssemblyInfo.cs`).toString();
+            assemblyInfo = assemblyInfo.replace(/AssemblyVersion\(\"([^\"]+)\"/, `AssemblyVersion("${version}.0"`);
+            terminal.writeFileSync(`./${folder}/Properties/AssemblyInfo.cs`, assemblyInfo);
+        } catch (e) { /* Ignore */ }
     }
 }
